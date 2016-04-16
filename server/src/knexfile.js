0e7e60f68bcebfc5eaa85e392f4450527abc673b
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2016-04-17 00:14:39 +0300

    correct connection migration pool

--- server/src/knexfile.js
+++ server/src/knexfile.js
@@ -1,7 +1,15 @@
+'use strict'
+
+let db_host = ''
+let db_user = ''
+let db_password = ''
+let db_name = 't'
+
+const DATABASE_URL = `postgres://${db_user}:${db_password}@${db_host}:5432/d${db_name}?ssl=true`
+
 module.exports = {
-  client: 'pg',
-  connection: process.env.DATABASE_URL,
-  debug: true,
+  client: 'postgresql',
+  connection: DATABASE_URL,
   pool: {
     min: 1,
     max: 7

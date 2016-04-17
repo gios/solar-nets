e57f36d6125f5621a33cf0a99f59dc72047114ff
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2016-04-17 11:28:41 +0300

    db connect credentials

--- server/src/knexConfig.js
+++ server/src/knexConfig.js
@@ -7,14 +7,9 @@ let db_name = 'dcu20n453qcuqt'
 
 const DATABASE_URL = `postgres://${db_user}:${db_password}@${db_host}:5432/${db_name}?ssl=true`
 
-module.exports = {
-  client: 'postgresql',
-  connection: DATABASE_URL,
-  pool: {
-    min: 1,
-    max: 7
-  },
-  migrations: {
-    tableName: 'knex_migrations'
-  }
-}
+let pg = require('knex')({
+  client: 'pg',
+  connection: DATABASE_URL
+})
+
+module.exports = pg

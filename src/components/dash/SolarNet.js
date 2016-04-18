cc71eff2f5fb9c8b64ef0e8b518ddbd0d76b7ebf
gios <pblazhchuk@gmail.com>
2016-04-18 14:31:26 +0300

    status point

--- src/components/dash/SolarNet.js
+++ src/components/dash/SolarNet.js
@@ -189,7 +189,8 @@ class SolarNet extends Component {
     return (
       <div className='text-xs-center'>
         <h5 className='iterations-counter'>
-          Iterations: <span className='label label-default'>{iterations}</span>
+          Iterations: <span className='label label-default'>{iterations}</span><br/>
+          Writing: <div className='point-status'></div>
         </h5>
         <div id='solar-petri-net'></div>
         {netRender && <NetControls simulation={simulation}

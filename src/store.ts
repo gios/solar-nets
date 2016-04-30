bc2613223fb5dab4f75694333d68a07068faf534
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2016-04-30 16:25:02 +0300

    fix store

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,12 @@
-import { createStore, compose, applyMiddleware } from 'redux'
-import { apiMiddleware } from 'redux-api-middleware'
-import thunk from 'redux-thunk'
+import {createStore, compose, applyMiddleware} from "redux";
+import {apiMiddleware} from "redux-api-middleware";
+import thunk from "redux-thunk";
 
 // Store
-import solarNets from './reducers'
+import solarNets from "./reducers";
 const store = createStore(solarNets, compose(
   applyMiddleware(thunk, apiMiddleware),
-  window.devToolsExtension ? window.devToolsExtension() : f => f
-))
+  window["devToolsExtension"] ? window["devToolsExtension"]() : f => f
+));
 
 export default store

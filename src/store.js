9f68b91932223580e808d16cce552b8c07258f69
Pavlo Blazhchuk <gios@users.noreply.github.com>
2016-05-02 20:33:38 +0300

    remove comments

--- src/store.js
+++ src/store.js
@@ -2,7 +2,6 @@ import { createStore, compose, applyMiddleware } from 'redux'
 import { apiMiddleware } from 'redux-api-middleware'
 import thunk from 'redux-thunk'
 
-// Store
 import solarNets from './reducers'
 const store = createStore(solarNets, compose(
   applyMiddleware(thunk, apiMiddleware),

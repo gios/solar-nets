b1e2dce3dd4bfca7004e7dc931915d0d561cbb7a
gios <pblazhchuk@gmail.com>
2016-05-05 16:07:13 +0300

    bump version 1.3.29

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const VERSION = 'Beta 1.3.27'
+export const VERSION = 'Beta 1.3.29'
 export const MOBILE_MAX_WIDTH = 1376
 export const CHART_INTERVAL_LIMIT = 12
 export const CHART_MAX_INTERVAL = 10000

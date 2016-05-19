70b21cbe23192dba48a90b136442e67b4cdd293c
Pavlo Blazhchuk <gios@users.noreply.github.com>
2016-05-19 22:35:47 +0300

    update

--- src/store.js
+++ src/store.js
@@ -3,6 +3,7 @@ import { apiMiddleware } from 'redux-api-middleware'
 import thunk from 'redux-thunk'
 
 import solarNets from './reducers'
+
 const store = createStore(solarNets, compose(
   applyMiddleware(thunk, apiMiddleware),
   window.devToolsExtension ? window.devToolsExtension() : f => f
